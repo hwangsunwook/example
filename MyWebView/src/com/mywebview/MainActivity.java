89c5d9fd302dd2637e83ffbe24685c07d2debad2
swhwang_pro <swhwang_pro@prodigycom.co.kr>
2014-08-08 09:16:35 +0900

    WebView
    WebView

---
+++ MyWebView/src/com/mywebview/MainActivity.java
@@ -0,0 +1,66 @@
+package com.mywebview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+	WebView mWebView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mWebView = (WebView)findViewById(R.id.webview);
+		mWebView.getSettings().setJavaScriptEnabled(true);
+//		mWebView.loadUrl("http://www.hearsayenglish.com");
+		mWebView.loadUrl("https://translate.google.co.kr/#en/ko/");
+		mWebView.setWebViewClient(new MyWebClient());
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+		
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+			mWebView.goBack();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	private class MyWebClient extends WebViewClient {
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			// TODO Auto-generated method stub
+			view.loadUrl(url);
+			return super.shouldOverrideUrlLoading(view, url);
+		}
+		
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
