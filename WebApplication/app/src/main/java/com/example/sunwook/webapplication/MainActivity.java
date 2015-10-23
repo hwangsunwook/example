35fd0f15bd2328f0907eaa9c3880e10c43820b36
hwangsunwook <burninghwang@gmail.com>
2015-10-23 16:36:46 +0900

    Update WebApplication for JAVA Scription
    Update WebApplication for JAVA Scription
    자사 스크립트 수행이 될 수 있도록 수정 함.

--- WebApplication/app/src/main/java/com/example/sunwook/webapplication/MainActivity.java
+++ WebApplication/app/src/main/java/com/example/sunwook/webapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.sunwook.webapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.webview);
+
+        final MyJavaScriptInterface myJavaScriptInterface
+                = new MyJavaScriptInterface(this);
+        mWebView.addJavascriptInterface(myJavaScriptInterface, "AndroidFunction");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
         mWebView.setWebChromeClient(new WebChromeClient());
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    public class MyJavaScriptInterface {
+        Context mContext;
+
+        MyJavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Log.d("TAG", "MyJavaScriptInterface");
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
